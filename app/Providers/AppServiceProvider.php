841768bbb7539c1919a4566fa9edce6f5f10d79a
Smash2222 <karbovskyi.volodymyr@gmail.com>
2022-11-24 19:39:54 +0200

    add front, setting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Model::preventLazyLoading(!app()->isProduction());
+        Model::preventsSilentlyDiscardingAttributes(!app()->isProduction());
+
+//        DB::whenQueryingForLongerThan(500, function (Connection $connection, QueryExecuted $event) {
+//            // TODO
+//        });
+
+        // TODO request cycle
     }
 }
